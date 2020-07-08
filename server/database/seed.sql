64a0cab94d9b0fc963b8eb654a6c3222b581c1a3
github username <ingrey1@gmail.com>
2020-07-07 20:33:02 -0700

    added initial schema, and db setup files

---
+++ server/database/seed.sql
@@ -0,0 +1,10 @@
+-- to provide seed data for DB, from server/database
+--  psql -h localhost -U postgres -d fitness_development -f ./seed.sql
+--  psql -h localhost -U postgres -d fitness_test -f ./seed.sql
+
+
+INSERT INTO users
+    (first_name, last_name, password_digest, admin, username, email, created_at, updated_at)
+VALUES
+    ('Jason', 'Mortensen', 'ASVAVDDG1d4345dg5675', true, 'ingrey1', 'ingrey1@gmail.com', NOW(), NOW());
+    

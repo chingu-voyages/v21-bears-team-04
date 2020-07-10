85eda570ac7272d361cf18e61cf807329061ed5f
github username <ingrey1@gmail.com>
2020-07-10 02:29:47 -0700

    updated example for using DBbase#query(q)

--- server/models/DBbase.js
+++ server/models/DBbase.js
@@ -5,29 +5,28 @@ class DBbase {
     this.connection = adapter;
   }
 
-
   async query(q) {
     return this.connection.query(q);
   }
 }
 
-const adapter = new DatabaseAdapter({
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    user: process.env.DB_USER,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD
-  });
+// uncomment example to test
 
+// const adapter = new DatabaseAdapter({
+//   host: process.env.DB_HOST,
+//   port: process.env.DB_PORT,
+//   user: process.env.DB_USER,
+//   database: process.env.DB_NAME,
+//   password: process.env.DB_PASSWORD
+// });
 
+// const dbBase = new DBbase(adapter);
 
-async function stuff() {
-    const res = await DBbase.query("SELECT NOW();")
-    console.log(res)
-}
+// async function test() {
+//   const result = await dbBase.query("SELECT NOW();");
+//   console.log(result);
+// }
 
-stuff();
-
-
+// test();
 
 module.exports = DBbase;

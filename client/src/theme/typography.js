a5ce9e5380e62e256b2ac3e8a5e7ec5e007c637b
Michael Chu <mcchu95@gmail.com>
2020-07-12 01:22:58 -0400

    Setup jss theming

---
+++ client/src/theme/typography.js
@@ -0,0 +1,75 @@
+export default {
+  fontFamily: '"Roboto", sans-serif;',
+  variants: {
+    h1: {
+      fontSize: '6rem',
+      fontWeight: 300,
+      lineHeight: 1.167,
+    },
+    h2: {
+      fontSize: '3.75rem',
+      fontWeight: 300,
+      lineHeight: 1.2,
+    },
+    h3: {
+      fontSize: '3rem',
+      fontWeight: 400,
+      lineHeight: 1.167,
+    },
+    h4: {
+      fontSize: '2.125rem',
+      fontWeight: 400,
+      lineHeight: 1.235,
+    },
+    h5: {
+      fontSize: '1.5rem',
+      fontWeight: 500,
+      lineHeight: 1.6,
+      letterSpacing: '1.25px',
+    },
+    h6: {
+      fontSize: '1.25rem',
+      fontWeight: 500,
+      lineHeight: 1.6,
+      letterSpacing: 0.2,
+    },
+    body1: {
+      fontSize: '1rem',
+      fontWeight: 400,
+      lineHeight: 1.5,
+      letterSpacing: '0.75px',
+    },
+    body2: {
+      fontSize: '0.875rem',
+      fontWeight: 400,
+      lineHeight: 1.43,
+      letterSpacing: '0.5px',
+    },
+    subtitle1: {
+      fontSize: '1rem',
+      fontWeight: 400,
+      lineHeight: 1.75,
+    },
+    subtitle2: {
+      fontSize: '0.875rem',
+      fontWeight: 500,
+      lineHeight: 1.57,
+    },
+    button: {
+      fontSize: '0.875rem',
+      lineHeight: 1.75,
+    },
+    caption: {
+      fontSize: '0.75rem',
+      fontWeight: 400,
+      lineHeight: 1.66,
+    },
+    overline: {
+      fontSize: '0.625rem',
+      fontWeight: 400,
+      textTransform: 'uppercase',
+      lineHeight: 2.33,
+      letterSpacing: '0.67px',
+    },
+  },
+};

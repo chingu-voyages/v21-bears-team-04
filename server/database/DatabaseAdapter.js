9e378fbda5d17ef328729a2df08ae100f8ea013a
github username <ingrey1@gmail.com>
2020-07-12 20:19:27 -0700

    refactored DatabaseAdapter

--- server/database/DatabaseAdapter.js
+++ server/database/DatabaseAdapter.js
@@ -1,104 +1,52 @@
 require("dotenv").config();
-const { Client } = require("pg");
 const { Pool } = require("pg");
 
 class DatabaseAdapter {
   constructor(connectionInfo) {
-    this.client = new Client(connectionInfo);
     this.pool = new Pool(connectionInfo);
     console.log("\nconnected to db\n");
   }
 
-  async query(q) {
-    // try {
-      const client = await this.pool.connect(); // grab new client from pool
-      const { rows } = await client.query(q);
-      client.release(); // return client to pool
-      return rows;
-    // } catch (err) {
-    //   console.log("couldn't connect to db", err);
-    // }
+  async query(q, values) {
+
+    const { rows } = await (values ? this.pool.query(q, values) : this.pool.query(q));
+    return rows;
   }
 
-  queryAndDisconnect(query, queryResultF) {
-    console.log("");
-    this.startConnection();
-    this.client.query(query, (err, res) => {
-      if (err) throw err;
-      queryResultF(res);
-      console.log();
-      this.endConnection();
-    });
-  }
-
-  listConnectedDBName() {
+  async listConnectedDBName() {
     const query = "SELECT current_database();";
-    const queryResultF = res => {
-      // list current db
-      const DBName = res.rows[0].current_database;
-      console.log(`current DB: ${DBName}`);
-    };
-    this.queryAndDisconnect(query, queryResultF);
+    const result = await this.query(query);
+    console.log(`\nCurrently connected to DB: ${result[0].current_database}\n`);
   }
 
-  listAllTableNames() {
+  async listAllTableNames() {
     const query =
       "SELECT table_name FROM information_schema.tables WHERE table_schema='public';";
-    const queryResultF = res => {
-      // list all the dbs
-      const tableNames = res.rows;
-      console.log("table names: \n");
-      tableNames.forEach(row => {
-        console.log(`  ${row.table_name}`);
-      });
-    };
-
-    this.queryAndDisconnect(query, queryResultF);
-  }
-
-  listPort() {
-    const query = "SELECT * FROM pg_settings WHERE name='port';";
-    const queryResultF = res => {
-      // list port
-      const portNumber = res.rows[0].setting;
-      console.log(`Port Number: ${portNumber}`);
-    };
-
-    this.queryAndDisconnect(query, queryResultF);
-  }
-
-  listDatabases() {
-    const query = "SELECT datname FROM pg_database;";
-    const queryResultF = res => {
-      //list all the dbs
-      console.log("Databases:\n");
-      res.rows.forEach(row => {
-        console.log(row.datname);
-      });
-    };
-    this.queryAndDisconnect(query, queryResultF);
-  }
-
-  startConnection() {
-    this.client.connect(err => {
-      if (err) {
-        console.error("connection error", err.stack);
-      } else {
-        console.log("connected\n");
-      }
+    const result = await this.query(query);
+    // list connected db table names
+    console.log("\ntable names: \n");
+    result.forEach(row => {
+      console.log(`  ${row.table_name}`);
     });
   }
 
-  endConnection() {
-    this.client.end(err => {
-      console.log("client has disconnected\n");
-      if (err) {
-        console.log("error during disconnection", err.stack);
-      }
+  async listPort() {
+    const query = "SELECT * FROM pg_settings WHERE name='port';";
+    const result = await this.query(query);
+    // list port
+    const portNumber = result[0].setting;
+    console.log(`\nPort Number: ${portNumber}\n`);
+  }
+
+  async listDatabases() {
+    const query = "SELECT datname FROM pg_database;";
+    const result = await this.query(query);
+    console.log("Databases:\n");
+    result.forEach(row => {
+      console.log(row.datname);
     });
   }
 }
-
 
 const adapter = new DatabaseAdapter({
   host: process.env.DB_HOST,
@@ -111,12 +59,14 @@ const adapter = new DatabaseAdapter({
 // example: uncomment below, add .env file in database folder with DB_HOST, DB_PORT etc.
 // from server/database run 'node DatabaseAdapter.js'
 
+async function test() {
+  adapter.listPort();
+  adapter.listDatabases();
+  adapter.listConnectedDBName();
+  adapter.listAllTableNames();
 
+}
 
-// async function test() {
-//   const result = await adapter.query("SELECT NOW();");
-// }
-
-// test();
+test();
 
 module.exports = adapter;

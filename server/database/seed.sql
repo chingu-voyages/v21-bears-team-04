949400ac843f018083cb9e9f58bef2e1ea874d5a
Tess <50762504+tcgronk@users.noreply.github.com>
2020-07-12 18:14:58 -0700

    Update seed.sql with consolidated statements

--- server/database/seed.sql
+++ server/database/seed.sql
@@ -9,36 +9,16 @@
 INSERT INTO users
     (first_name, last_name, password_digest, admin, username, email, created_at, updated_at)
 VALUES
-    ('Jason', 'Mortensen', 'ASVAVDDG1d4345dg5675', true, 'ingrey1', 'ingrey1@gmail.com', NOW(), NOW());
-
-INSERT INTO users
-    (first_name, last_name, password_digest, admin, username, email, created_at, updated_at)
-VALUES
-    ('Bear', 'Cat', 'BSVAVDDG1d4345dg5675', false, 'bear1', 'bear1@gmail.com', NOW(), NOW());
-
-INSERT INTO users
-    (first_name, last_name, password_digest, admin, username, email, created_at, updated_at)
-VALUES
+    ('Jason', 'Mortensen', 'ASVAVDDG1d4345dg5675', true, 'ingrey1', 'ingrey1@gmail.com', NOW(), NOW()),
+    ('Bear', 'Cat', 'BSVAVDDG1d4345dg5675', false, 'bear1', 'bear1@gmail.com', NOW(), NOW()),
     ('Kuma', 'Cat', 'CSVAVDDG1d4345dg5675', false, 'kuma1', 'kuma1@gmail.com', NOW(), NOW());
 
 
 
-
-
-
-
 INSERT INTO addresses
     (country, city, postal_code, user_id, created_at, updated_at)
 VALUES
-    ('US', 'San Francisco', 44444, 1, NOW(), NOW());
-
-INSERT INTO addresses
-    (country, city, postal_code, user_id, created_at, updated_at)
-VALUES
-    ('RU', 'Moscow', 55555, 4, NOW(), NOW());
-
-INSERT INTO addresses
-    (country, city, postal_code, user_id, created_at, updated_at)
-VALUES
+    ('US', 'San Francisco', 44444, 1, NOW(), NOW()),
+    ('RU', 'Moscow', 55555, 4, NOW(), NOW()),
     ('NZ', 'Aukland', 66666, 5, NOW(), NOW());
 

896456e45ac2a1a55997c901b4e1cf1b796071e0
github username <ingrey1@gmail.com>
2020-07-18 15:47:11 -0700

    updated reducer

--- client/src/reducers/journal.js
+++ client/src/reducers/journal.js
@@ -1,5 +1,8 @@
 import { SET_JOURNAL } from "../actions/types";
 
+// this reducer is responsible for handling all the activities object that belong to the users journal
+// so, any activities that the user logs
+
 const initialState = [];
 
 function journal(state = initialState, action) {

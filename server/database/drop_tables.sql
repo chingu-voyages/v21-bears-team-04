634c9e8404d9d5711f1a1fe188284b78d4ecb022
github username <ingrey1@gmail.com>
2020-07-18 10:07:09 -0700

    schema update

--- server/database/drop_tables.sql
+++ server/database/drop_tables.sql
@@ -1,3 +1,14 @@
 ALTER TABLE "addresses" DROP CONSTRAINT IF EXISTS "addresses_fk0";
 
+ALTER TABLE "activities" DROP CONSTRAINT IF EXISTS "activities_fk0";
+
+ALTER TABLE "activities" DROP CONSTRAINT IF EXISTS "activities_fk1";
+
+
 DROP TABLE IF EXISTS "addresses";
+
+DROP TABLE IF EXISTS "activities";
+
+DROP TABLE IF EXISTS "activity_categories";
+
+DROP TABLE IF EXISTS "users";

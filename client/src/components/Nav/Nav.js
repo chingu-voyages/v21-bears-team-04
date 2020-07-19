22ad06789c6360dac95a059230d180c8eda3d2bc
Tess Gronski <tessgronski@gmail.com>
2020-07-19 14:31:14 -0700

    Formats changes for PR

--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import './Nav.css'
+import React from "react";
+import "./Nav.css";
 //public homepage header
 
 function Nav() {
   return (
     <div className="Nav">
       <h1 className="App-title">FitX</h1>
-      <div className='App-account'>
-      <p>Login</p>
-      <p>Register</p>
+      <div className="App-account">
+        <p>Login</p>
+        <p>Register</p>
       </div>
     </div>
   );
--- client/src/containers/App/App.js
+++ client/src/containers/App/App.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import Nav from '../../components/Nav/Nav';
-import './App.css';
-import Footer from '../../components/Footer/Footer';
+import React from "react";
+import Nav from "../../components/Nav/Nav";
+import "./App.css";
+import Footer from "../../components/Footer/Footer";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Nav/>
+        <Nav />
       </header>
-      <Footer/>
+      <Footer />
     </div>
   );
 }

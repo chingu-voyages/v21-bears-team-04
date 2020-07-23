e7cd9af1563d808a2902796c6b717d1786572f51
Jason Mortensen <zaross1@yahoo.com>
2020-07-23 01:24:30 -0700

    updated frontend api method getDashboard

--- client/src/containers/App/App.js
+++ client/src/containers/App/App.js
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import Nav from '../../components/Nav/Nav';
 import { Footer } from '../../components';
 import Homepage from '../../components/Homepage/Homepage';
+// import {getDashboard} from "../../api/api"
+
 
 function App() {
+  // const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NCwidXNlcm5hbWUiOiJ1c2VyMSIsImVtYWlsIjoidXNlcjFAZ21haWwuY29tIiwiaWF0IjoxNTk1NDg2Mzk1LCJleHAiOjE1OTU1MDc5OTV9.K017NZcNh3AcAhnVPV6_cK0gLVnNJlR-WPaqfMKviYA"
+  // useEffect( ()=> {
+    
+
+  //     getDashboard(2, token).then(data => console.log("data", data["myActivities"][0])).catch(err => console.log(err))
+      
+
+    
+    
+  // }, [])
+  
+
   return (
     <div className="App">
       <header className="App-header">

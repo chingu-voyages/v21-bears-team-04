081ba9bc6503672f67738152e6ef366042833920
Jason Mortensen <zaross1@yahoo.com>
2020-07-24 19:06:28 -0700

    added getCategoryForActivity(activityCategoryId, activityCategories)

--- client/src/utils/transformations.js
+++ client/src/utils/transformations.js
@@ -1,15 +1,17 @@
-
-
-
 const getActivitiesCreatedByUser = (activities, userId) => {
-    return activities.filter(activity => userId === activity.user_id)
-}
+  return activities.filter((activity) => userId === activity.user_id);
+};
 
 const getLikesForActivity = (activityId, likes) => {
-    return likes.filter(like => activity.Id === likes.resource_id) 
-}
+  return likes.filter((like) => activity.Id === likes.resource_id);
+};
 
-const getCommentsForActivity = (commentId, comments) => {
-    return comments.filter(comment => comment.Id === comment.resource_id) 
-}
+const getCommentsForActivity = (activityId, comments) => {
+  return comments.filter((comment) => activityId === comment.resource_id);
+};
 
+const getCategoryForActivity = (activityCategoryId, activityCategories) => {
+  return activityCategories.find(
+    (category) => activityCategoryId === category.id
+  );
+};

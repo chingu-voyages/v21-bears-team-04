17548fa2cf122cb3a3b98d5cca1e814b176d438a
Jason Mortensen <zaross1@yahoo.com>
2020-07-28 16:03:25 -0700

    updated getDashboard action in feed, and api.feed.getDashboard

--- client/src/actions/feed.js
+++ client/src/actions/feed.js
@@ -1,1 +1,25 @@
-export const getDashboard = async () => {};
+// import {
+//   SET_ACTIVITIES,
+//   SET_ACTIVITY_CATEGORIES,
+//   SET_COMMENTS,
+//   SET_FOLLOWINGS,
+//   SET_LIKES,
+//   SET_USERS
+// } from "./types";
+import api from "../services";
+import { setActivities } from "./activities";
+import { setActivityCategories } from "./activityCategories";
+import { setComments } from "./comments";
+import { setFollowings } from "./following";
+import { setLikes } from "./likes";
+import { setUsers } from "./users";
+
+export const getDashboard = (token) => async (dispatch) => {
+  try {
+    const dashboardData = await api.feed.getDashboard(token);
+    console.log("dashboarddata", dashboardData)
+
+  } catch (err) {
+      console.log(err)
+  }
+};

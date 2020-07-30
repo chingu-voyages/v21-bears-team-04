d42d6a0f9b686c51ec3865f50985f2d4e2fa0130
Jason Mortensen <zaross1@yahoo.com>
2020-07-30 13:16:56 -0700

    added dropdown for category select in AddActivity component

--- client/src/containers/AddActivity.js
+++ client/src/containers/AddActivity.js
@@ -1,18 +1,41 @@
 import React from "react";
 import { useFormik } from "formik";
 import { connect } from "react-redux";
+import Select from "@material-ui/core/Select";
+import InputLabel from "@material-ui/core/InputLabel";
+import MenuItem from "@material-ui/core/MenuItem";
 
-export const _AddActivity = () => {
+export const _AddActivity = ({categories}) => {
   const { handleChange, values, handleSubmit } = useFormik({
-    initialValues: {},
+    initialValues: {category: categories[0].id},
     onSubmit: (values) => {},
   });
+
+  const renderCategoryChoices = (categories) => {
+    return categories.map(category => <MenuItem value={category.id}>{category.name}</MenuItem>)
+  }
 
   return (
     <div>
       <h1>Add Activity</h1>
+      <form onSubmit={handleSubmit}>
+        <InputLabel id="activity-category">Activity</InputLabel>
+        <Select
+         labelId="activity-category"
+          id="activity-category-select"
+          value={values.category}
+          onChange={handleChange}
+        >
+           
+          {renderCategoryChoices(categories)}
+        </Select>
+      </form>
     </div>
   );
 };
 
-export const AddActivity = connect(null, {})(_AddActivity);
+const mapStateToProps = (state) => {
+  return { categories: state.activityCategories };
+};
+
+export const AddActivity = connect(mapStateToProps, {})(_AddActivity);

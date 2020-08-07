37694e4e67fcf63e7ab293e940236c3ef67ed7b2
Jason Mortensen <zaross1@yahoo.com>
2020-08-06 19:39:33 -0700

    updated chart

--- client/src/containers/Dashboard/Chart.js
+++ client/src/containers/Dashboard/Chart.js
@@ -1,19 +1,26 @@
 import React from "react";
-// import BarChart from "react-bar-chart";
+import BarChart from "react-bar-chart";
 const Chart = () => {
-  const data = {
-    monday: 1100,
-    tuesday: 500,
-    wednesday: 4400,
-    thursday: 1000,
-    friday: 2200,
-    saturday: 300,
-    sunday: 500,
-  };
+  const data = [
+    { text: "monday", value: 1100 },
+    { text: "tuesday", value: 500 },
+    { text: "wednesday", value: 4400 },
+    { text: "thursday", value: 1000 },
+    { text: "friday", value: 2200 },
+    { text: "saturday", value: 300 },
+    { text: "sunday", value: 500 },
+  ];
   const margin = { top: 20, right: 20, bottom: 30, left: 40 };
   return (
     <div>
-      {/* <BarChart ylabel="Calories" margin={margin} data={data} /> */}chart
+      <BarChart
+        ylabel="Calories"
+        xlabel="Days"
+        height={500}
+        width={800}
+        margin={margin}
+        data={data}
+      />
     </div>
   );
 };

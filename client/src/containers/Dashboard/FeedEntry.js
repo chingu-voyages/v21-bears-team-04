89f228327d3be08eebd37fc954986389e4268bcb
Jason Mortensen <zaross1@yahoo.com>
2020-08-06 18:35:12 -0700

    update

--- client/src/containers/Dashboard/FeedEntry.js
+++ client/src/containers/Dashboard/FeedEntry.js
@@ -1,7 +1,7 @@
 import React from "react";
 
-const FeedEntry = () => {
-  return <div>FeedEntry</div>;
+const FeedEntry = ({ activity }) => {
+  return <div>{activity.title}</div>;
 };
 
 export default FeedEntry;

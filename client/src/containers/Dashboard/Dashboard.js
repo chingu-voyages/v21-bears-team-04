02955096c6acbe2605ba8bfbb690da693a4b162c
Jason Mortensen <zaross1@yahoo.com>
2020-08-06 18:11:42 -0700

    added stubs for Dashboard sub-components: Feed, Charts

---
+++ client/src/containers/Dashboard/Dashboard.js
@@ -0,0 +1,50 @@
+import React, { useEffect } from "react";
+import { connect } from "react-redux";
+import { getDashboard } from "../../actions/feed";
+import { Typography } from "../../components";
+import { createUseStyles } from "react-jss";
+import Feed from "./Feed";
+import Charts from "./Charts";
+
+export const _Dashboard = ({ auth: { token }, getDashboard }) => {
+  const classes = useStyles();
+  useEffect(() => {
+    console.log("token", token);
+    getDashboard(token);
+  }, [token]);
+
+  return (
+    <div className={classes.root}>
+      <Typography variant="h1" className={classes.header}>
+        Dashboard
+      </Typography>
+    </div>
+  );
+};
+
+const mapStateToProps = (state) => {
+  return {
+    auth: state.auth,
+  };
+};
+
+export const Dashboard = connect(mapStateToProps, { getDashboard })(_Dashboard);
+
+export const styles = (theme) => ({
+  root: {
+    margin: theme.spacing(4, 0),
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+
+    "& > div": {
+      margin: theme.spacing(4, 0),
+    },
+  },
+  header: {
+    marginBottom: "15px",
+  },
+});
+
+const useStyles = createUseStyles(styles, { name: "dashboard" });

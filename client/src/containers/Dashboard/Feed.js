b15cc8bb4946c0760df01e7087efa5bf160af1ac
Jason Mortensen <zaross1@yahoo.com>
2020-08-06 18:33:07 -0700

    added getFeedActivities util function

--- client/src/containers/Dashboard/Feed.js
+++ client/src/containers/Dashboard/Feed.js
@@ -1,10 +1,30 @@
-import React from 'react'
-import {Row, Col} from 'react-bootstrap'
-import {connect} from 'react-redux'
+import React from "react";
+import { Row, Col } from "react-bootstrap";
+import FeedEntry from "./FeedEntry";
+import {getFeedActivities} from "../../utils/transformations"
+import { connect } from "react-redux";
 
+const Feed = ({ activities, following, userId }) => {
+  const renderFeedEntries = (entries) => {
+    return entries.map((activity) => (
+      <Row>
+        <FeedEntry activity={activity} />
+      </Row>
+    ));
+  };
 
-const Feed = () => {
-    return <Col>Feed</Col>
-}
+  return (
+    
+    <Col>{activities && renderFeedEntries(getFeedActivities(userId, activities, following))}</Col>
+  );
+};
 
-export default Feed
+const mapStateToProps = (state) => {
+  return {
+    activities: state.activities,
+    following: state.following,
+    userId: state.auth.userId,
+  };
+};
+
+export default connect(mapStateToProps)(Feed);

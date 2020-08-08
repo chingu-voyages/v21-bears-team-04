98b5a94b1a1ee4d07ec682b94f81c24f2578114e
Jason Mortensen <zaross1@yahoo.com>
2020-08-07 18:58:51 -0700

    added detailsIcon to feedentries

--- client/src/containers/Dashboard/FeedEntry.js
+++ client/src/containers/Dashboard/FeedEntry.js
@@ -5,8 +5,9 @@ import { createUseStyles } from "react-jss";
 const FeedEntry = ({ activity, user, category }) => {
   const classes = useStyles();
   return (
-    <div clasSName={classes.container}>
-      <Avatar size="small" src="/images/default_avatar_image.png" />
+    <div className={classes.container}>
+      <Avatar size="small" src="images/default_avatar_image.png" />
+      <div className={classes.detailsIcon}></div>
       <h6 className={classes.username}>{user.username}</h6>
       <h5 className={classes.title}>{activity.title}</h5>
       <h6>{category && category.name}</h6>
@@ -25,6 +26,14 @@ export const styles = (theme) => ({
   },
   container: {
     width: "100%",
+  },
+
+  detailsIcon: {
+    width: "20px",
+    height: "20px",
+    background: `url(${"/images/details_icon.png"}) no-repeat center/cover`,
+    float: "right",
+    marginRight: "5px",
   },
 });
 
